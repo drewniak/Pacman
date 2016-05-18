6cb18f2b0b579a50302fb71981b5a78f4a6cf95d
Aleksandra Drewniak <aleksandra.drewniak1a@gmail.com>
2016-05-18 20:52:49 +0200

    First commit - added libraries and created simple labyrinth

---
+++ main.js
@@ -0,0 +1,205 @@
+
+var map = [ // 1  2  3  4  5  6  7  8  9
+           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 0
+           [1, 1, 0, 0, 0, 0, 0, 1, 1, 1,], // 1
+           [1, 1, 0, 0, 2, 0, 0, 0, 0, 1,], // 2
+           [1, 0, 0, 0, 0, 2, 0, 0, 0, 1,], // 3
+           [1, 0, 0, 2, 0, 0, 2, 0, 0, 1,], // 4
+           [1, 0, 0, 0, 2, 0, 0, 0, 1, 1,], // 5
+           [1, 1, 1, 0, 0, 0, 0, 1, 1, 1,], // 6
+           [1, 1, 1, 0, 0, 1, 0, 0, 1, 1,], // 7
+           [1, 1, 1, 1, 1, 1, 0, 0, 1, 1,], // 8
+           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 9
+           ], mapW = map.length, mapH = map[0].length;
+
+// Semi-constants
+var WIDTH = window.innerWidth,
+	HEIGHT = window.innerHeight,
+	ASPECT = WIDTH / HEIGHT,
+	UNITSIZE = 250,
+	WALLHEIGHT = UNITSIZE,
+	MOVESPEED = 100,
+	LOOKSPEED = 0.025,
+	BULLETMOVESPEED = MOVESPEED * 5,
+	NUMAI = 5,
+	PROJECTILEDAMAGE = 20;
+// Global vars
+var t = THREE, scene, cam, renderer, controls, clock, projector, model, skin;
+var runAnim = true, mouse = { x: 0, y: 0 }, kills = 0, health = 100;
+var pacman;
+var sphere, sphereGeometry, sphereMaterial;
+
+
+
+// Initialize and run on document ready
+$(document).ready(function() {
+	$('body').append('<div id="intro"> </div>');
+	$('#intro').css({width: WIDTH, height: HEIGHT}).one('click', function(e) {
+		e.preventDefault();
+		$(this).fadeOut();
+		init();
+		//setInterval(drawRadar, 1000);
+		animate();
+	});
+
+});
+
+
+// Setup
+function init() {
+	//pacman = new THREEx.Pacman();
+	//pacman.position.x= -1.5;
+	clock = new t.Clock(); // Used in render() for controls.update()
+	projector = new t.Projector(); // Used in bullet projection
+	scene = new t.Scene(); // Holds all objects in the canvas
+	scene.fog = new t.FogExp2(0xD6F1FF, 0.0005); // color, density
+	
+	// Set up camera
+	//cam = new t.PerspectiveCamera(60, ASPECT, 1, 10000); // FOV, aspect, near, far
+	cam = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+	cam.position.y = UNITSIZE * .2;
+	scene.add(cam);
+	//scene.add(pacman);
+	
+	sphereGeometry = new THREE.SphereGeometry(15, 15, 15);
+	sphereMaterial = new THREE.MeshLambertMaterial({color: 0xff3399});
+	sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	scene.add(sphere);
+	
+
+		
+				//scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+
+	
+	
+	
+	
+	// Camera moves with mouse, flies around with WASD/arrow keys
+	controls = new t.FirstPersonControls(cam);
+	controls.movementSpeed = MOVESPEED;
+	controls.lookSpeed = LOOKSPEED;
+	controls.lookVertical = false; // Temporary solution; play on flat surfaces only
+	controls.noFly = true;
+
+	// World objects
+	setupScene();
+	
+	// Handle drawing as WebGL (faster than Canvas but less supported)
+	renderer = new t.WebGLRenderer();
+	renderer.setSize(WIDTH, HEIGHT);
+	
+	// Add the canvas to the document
+	renderer.domElement.style.backgroundColor = '#D6F1FF'; // easier to see
+	document.body.appendChild(renderer.domElement);
+	
+	// Track mouse position so we know where to shoot
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+}
+
+// Helper function for browser frames
+function animate() {
+	if (runAnim) {
+		requestAnimationFrame(animate);
+	}
+	render();
+}
+
+// Update and display
+function render() {
+	var delta = clock.getDelta(), speed = delta * BULLETMOVESPEED;
+	var aispeed = delta * MOVESPEED;
+	controls.update(delta); // Move camera
+	renderer.render(scene, cam); // Repaint
+}
+
+// Set up the objects in the world
+function setupScene() {
+	var UNITSIZE = 250, units = mapW;
+
+	// Geometry: floor
+	var floor = new t.Mesh(
+			new t.CubeGeometry(units * UNITSIZE, 10, units * UNITSIZE),
+			new t.MeshLambertMaterial({color: 0xEDCBA0,/*map: t.ImageUtils.loadTexture('images/floor-1.jpg')*/})
+	);
+	scene.add(floor);
+	
+	// Geometry: walls
+	var cube = new t.CubeGeometry(UNITSIZE, WALLHEIGHT, UNITSIZE);
+	var materials = [
+	                 new t.MeshLambertMaterial({/*color: 0x00CCAA,*/map: t.ImageUtils.loadTexture('images/wall-1.jpg')}),
+	                 new t.MeshLambertMaterial({/*color: 0xC5EDA0,*/map: t.ImageUtils.loadTexture('images/wall-2.jpg')}),
+	                 new t.MeshLambertMaterial({color: 0xFBEBCD}),
+	                 ];
+	for (var i = 0; i < mapW; i++) {
+		for (var j = 0, m = map[i].length; j < m; j++) {
+			if (map[i][j]) {
+				var wall = new t.Mesh(cube, materials[map[i][j]-1]);
+				wall.position.x = (i - units/2) * UNITSIZE;
+				wall.position.y = WALLHEIGHT/2;
+				wall.position.z = (j - units/2) * UNITSIZE;
+				scene.add(wall);
+			}
+		}
+	}	
+	// Lighting
+	var directionalLight1 = new t.DirectionalLight( 0xF70FBE, 0.7 );
+	directionalLight1.position.set( 0.5, 1, 0.5 );
+	scene.add( directionalLight1 );
+	var directionalLight2 = new t.DirectionalLight( 0xF7EFBE, 0.5 );
+	directionalLight2.position.set( -0.5, -1, -0.5 );
+	scene.add( directionalLight2 );
+}
+
+function getMapSector(v) {
+	var x = Math.floor((v.x + UNITSIZE / 2) / UNITSIZE + mapW/2);
+	var z = Math.floor((v.z + UNITSIZE / 2) / UNITSIZE + mapW/2);
+	return {x: x, z: z};
+}
+
+function checkWallCollision(v) {
+	var c = getMapSector(v);
+	return map[c.x][c.z] > 0;
+}
+
+
+var bullets = [];
+var sphereMaterial = new t.MeshBasicMaterial({color: 0x333333});
+var sphereGeo = new t.SphereGeometry(2, 6, 6);
+
+function onDocumentMouseMove(e) {
+	e.preventDefault();
+	mouse.x = (e.clientX / WIDTH) * 2 - 1;
+	mouse.y = - (e.clientY / HEIGHT) * 2 + 1;
+}
+
+// Handle window resizing
+$(window).resize(function() {
+	WIDTH = window.innerWidth;
+	HEIGHT = window.innerHeight;
+	ASPECT = WIDTH / HEIGHT;
+	if (cam) {
+		cam.aspect = ASPECT;
+		cam.updateProjectionMatrix();
+	}
+	if (renderer) {
+		renderer.setSize(WIDTH, HEIGHT);
+	}
+	$('#intro, #hurt').css({width: WIDTH, height: HEIGHT,});
+});
+
+// Stop moving around when the window is unfocused (keeps my sanity!)
+$(window).focus(function() {
+	if (controls) controls.freeze = false;
+});
+$(window).blur(function() {
+	if (controls) controls.freeze = true;
+});
+
+//Get a random integer between lo and hi, inclusive.
+//Assumes lo and hi are integers and lo is lower than hi.
+function getRandBetween(lo, hi) {
+ return parseInt(Math.floor(Math.random()*(hi-lo+1))+lo, 10);
+}
+
+
+
